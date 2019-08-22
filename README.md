33dd78dc47d770debe2f4bbf5416c60a9809bb8b
Christopher Collins <chris@ccollins.io>
2019-08-21 23:42:45 -0600

    Created empy templates for all files

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # Word-Guess-Game
+
+This is a word guessing game like the traditional hangman. The goal is to guess the correct word if you guess correctly you win the round and go up one point. Wins and Losses are kept track of and refreshing the pages restarts the game.
+
+### Useful Links
+* This Live Repo: [https://ccollins1544.github.io/Word-Guess-Game](https://ccollins1544.github.io/Word-Guess-Game)
+* My Github Portal: [https://ccollins1544.github.io](https://ccollins1544.github.io/)
+
+## Contact
+* Email: **chris@ccollins.io**
---
+++ assets/javascript/game.js
@@ -0,0 +1,24 @@
+/**
+ * Game JS
+ * 
+ * @package Word Guess Game
+ * @author Christopher Collins
+ * @version 1.1.1
+ * @license none (public domain)
+ * 
+ * ===============[ TABLE OF CONTENTS ]===============
+ * 1.0 Backend
+ *   1.1 Initalize Variables
+ * 
+ * 2.0 Frontend
+ *   2.1
+ * 
+ *****************************************************/
+/*===============[ 1.0 Backend ]====================*/
+/* 1.1 Initialize Variables
+/*--------------------------------------------------*/
+var words = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+
+/*===============[ 2.0 Frontend]====================*/
+/* 2.1 
+/*--------------------------------------------------*/
